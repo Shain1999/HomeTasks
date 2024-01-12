bd04239dae2e8b4e828bee10744ffd5fc977f549
unknown <shain101@gmail.com>
2024-01-12 17:17:54 +0200

    after adding button to add task

--- lib/src/features/tasks/presentation/pages/home_page.dart
+++ lib/src/features/tasks/presentation/pages/home_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hometasks/src/features/tasks/domain/entities/task_category.dart';
 import 'package:hometasks/src/features/tasks/domain/entities/task_entity.dart';
+import 'package:hometasks/src/features/tasks/domain/entities/task_priority.dart';
+import 'package:hometasks/src/features/tasks/domain/entities/task_recurring.dart';
+import 'package:hometasks/src/features/tasks/domain/entities/task_reminders.dart';
 import 'package:hometasks/src/features/tasks/presentation/bloc/task_bloc.dart';
+import 'package:hometasks/src/features/tasks/presentation/bloc/task_event.dart';
 import 'package:hometasks/src/features/tasks/presentation/bloc/task_state.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,11 +16,12 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 class _HomeScreenState extends State<HomeScreen> {
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Title"),),
+      appBar: AppBar(title: Text("Title"),actions: [ElevatedButton(onPressed:(){_showAddTaskForm(context);}, child: Icon(Icons.add))],),
       body: BlocConsumer<TaskViewBloc, TasksViewState>(
         builder: (context, state) {
           if (state.status == TasksViewStatus.initial) {
@@ -45,6 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ScaffoldMessenger.of(context)
                 .showSnackBar(const SnackBar(content: Text("Operation Success")));
           }
+
         },
       ),
     );
@@ -56,6 +63,50 @@ class _HomeScreenState extends State<HomeScreen> {
       title: Text(task.title),
       subtitle: Text(task.isCompleted ? "Completed" : "Ongoing"),
       // Add more widgets as needed
+    );
+  }
+  void _showAddTaskForm(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _titleController,
+                  decoration: InputDecoration(labelText: 'Task Title'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a task title.';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    const Task testTask = Task(id: '15re32##489',
+                        title: 'task3',
+                        priority: TaskPriority.medium,
+                        reminders: TaskReminders.daily,
+                        category: TaskCategory.shopping,
+                        reccuring: TaskReccuring.mountly);
+                    context
+                        .read<TaskViewBloc>()
+                        .add(const OnAddTask(testTask));
+                  },
+                  child: Text('Add Task'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
